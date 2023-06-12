541c8d8122baffa94851dc387c408a9c86cd2a5d
FNH Rasthol <founoukossiherve@gmail.com>
2023-06-13 00:04:52 +0200

    premier envoi sur internet

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>ABOUT</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum exercitationem nemo debitis corrupti temporibus corporis facilis aperiam ea officiis porro, ut autem architecto quis obcaecati modi, rem alias tempore itaque accusamus. Ab, quasi odio. Molestias consectetur quod earum necessitatibus, soluta quo? Similique odit ullam dolores! In doloremque quis vero animi labore sapiente libero eveniet, sunt quos praesentium perferendis, dicta inventore. Mollitia aspernatur reiciendis quod impedit velit repudiandae quam nobis dolor obcaecati ducimus modi accusamus ex vero aliquam reprehenderit doloremque nesciunt odio expedita consectetur ipsam, magnam tempore dignissimos? Eos aliquam, nihil delectus odio debitis in cupiditate mollitia dolorum sequi, veniam fuga.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus cum autem perspiciatis obcaecati incidunt sequi ipsum quae eveniet? Accusantium adipisci, assumenda nihil officia quas sunt minima illum. Deserunt, officiis incidunt! Nihil molestias, error temporibus corporis quidem consectetur maxime a tenetur laudantium, dolor excepturi ipsa sit voluptatem velit autem voluptas officia culpa labore deleniti neque commodi hic eius nobis? Vero vel facere earum molestiae distinctio optio nihil odit corrupti, fugiat iure est in nostrum necessitatibus porro veniam excepturi ab nulla maxime omnis explicabo. Ipsa totam provident debitis quam quibusdam, quaerat odio qui similique nam? In reiciendis nesciunt quasi neque dicta, quam molestiae nisi mollitia architecto ipsum molestias possimus repudiandae debitis fugit minima ad eum dolorum at perspiciatis ipsa a laboriosam dolore voluptatem commodi. Laborum tenetur in non aliquid nostrum reprehenderit itaque aliquam provident accusamus hic, perspiciatis dolore quam ipsum molestias minima magnam id, possimus expedita cumque maxime ad atque iure! Magni quos deleniti eum rerum, neque animi est aperiam recusandae, necessitatibus, quo ratione enim impedit minima nulla laborum quidem? Laudantium natus perferendis ut, modi rem quas deleniti sunt debitis velit, nihil quo excepturi! Dolores, nihil. Distinctio, assumenda omnis placeat temporibus vero libero quaerat sequi impedit quis numquam culpa dignissimos corporis deleniti.</p>
+        </div>
+    );
+};
+
+export default About;
